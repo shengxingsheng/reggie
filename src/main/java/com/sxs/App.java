fdcdb8e99ff969f748ac23594777cbf488e26e2c
sxs <sxs@qq.com>
2022-11-23 22:06:11 +0800

    环境搭建

---
+++ src/main/java/com/sxs/App.java
@@ -0,0 +1,18 @@
+package com.sxs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Hello world!
+ */
+@Slf4j
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+        log.info("启动成功");
+    }
+}
